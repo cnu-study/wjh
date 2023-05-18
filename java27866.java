5681529ca6d497d7a335b73a3d213a12491c699e
woojunghyun <jhjh1664@naver.com>
2023-05-19 01:39:11 +0900

    5

---
+++ java27866.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class java27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String S = sc.next();
+        int i = sc.nextInt();
+        char a = S.charAt(i-1);
+        System.out.println(a);
+
+    }
+}
