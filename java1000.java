420ed61a3eee0a51b39bef2622839aeae992cd7f
woojunghyun <jhjh1664@naver.com>
2023-05-18 15:18:19 +0900

    1

---
+++ java1000.java
@@ -0,0 +1,18 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import java.util.Scanner;
+
+public class java1000 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a + b);
+        sc.close();
+    }
+}
